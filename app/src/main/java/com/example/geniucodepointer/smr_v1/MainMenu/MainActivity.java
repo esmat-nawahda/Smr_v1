3e1e976aadc299cb16edbeeec5f4b8bd9551e1a4
hamody.abed.m@gmail.com <hamody.abed.m@gmail.com>
2015-04-25 15:01:51 +0300

    Adding fragment

--- app/src/main/java/com/example/geniucodepointer/smr_v1/MainMenu/MainActivity.java
+++ app/src/main/java/com/example/geniucodepointer/smr_v1/MainMenu/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.geniucodepointer.smr_v1.MainMenu;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.geniucodepointer.smr_v1.Actions.Actions;
+import com.example.geniucodepointer.smr_v1.Actions.DoActions;
+import com.example.geniucodepointer.smr_v1.Actions.Settings;
 import com.example.geniucodepointer.smr_v1.R;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+    public  Actions actions = new DoActions();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +29,50 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        initlized();
         return true;
     }
+
+    public void initlized(){
+        Button edit_Button = (Button) findViewById(R.id.edit_button);
+        Button setting_Button = (Button) findViewById(R.id.setting_button);
+        Button friends_Button = (Button) findViewById(R.id.friends_button);
+
+
+
+        edit_Button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                actions.editImage();
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        setting_Button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Settings setting = new Settings();
+                actions.settings(getFragmentManager(),setting);
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        friends_Button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                actions.getFriends();
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -38,4 +88,10 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
 }
